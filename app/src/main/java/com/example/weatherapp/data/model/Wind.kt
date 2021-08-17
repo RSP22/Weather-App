65a228611f26c77411d7cd58e533ae0f6d3a930a
RSP22 <rudrik22@gmail.com>
2021-08-18 00:35:25 +0530

    new initial commit

---
+++ app/src/main/java/com/example/weatherapp/data/model/Wind.kt
@@ -0,0 +1,6 @@
+package com.example.weatherapp.data.model
+import java.io.Serializable
+data class Wind(
+    val speed: Double,
+    val deg: Int
+) : Serializable
---
+++ app/src/main/java/com/example/weatherapp/data/source/repository/WeatherRepository.kt
@@ -0,0 +1,23 @@
+package com.example.weatherapp.data.source.repository
+
+import com.example.weatherapp.data.model.LocationModel
+import com.example.weatherapp.data.model.Weather
+import com.example.weatherapp.data.model.WeatherForecast
+import com.example.weatherapp.utils.Result
+
+interface WeatherRepository {
+
+    suspend fun getWeather(location: LocationModel, refresh: Boolean): Result<Weather?>
+
+    suspend fun getForecastWeather(cityId: Int, refresh: Boolean): Result<List<WeatherForecast>?>
+
+    suspend fun getSearchWeather(location: String): Result<Weather?>
+
+    suspend fun storeWeatherData(weather: Weather)
+
+    suspend fun storeForecastData(forecasts: List<WeatherForecast>)
+
+    suspend fun deleteWeatherData()
+
+    suspend fun deleteForecastData()
+}
